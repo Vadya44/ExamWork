c63c05551f45c080fc453939ccae97dd8644b68d
Вадим Гатауллин <Vadya@macbook-pro-vadim-2.local>
2019-03-25 14:03:12 +0300

    Initial Commit

---
+++ ExamWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamWork
+//
+//  Created by Вадим Гатауллин on 25/03/2019.
+//  Copyright © 2019 Вадим Гатауллин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
