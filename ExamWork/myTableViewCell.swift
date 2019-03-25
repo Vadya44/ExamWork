40b5ad28dff87a23eab4e6d8afdd7a9f93116c35
Вадим Гатауллин <Vadya@MacBook-Pro-Vadim-2.local>
2019-03-25 19:27:54 +0300

    first commit

---
+++ ExamWork/myTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableViewCell.swift
+//  ExamWork
+//
+//  Created by Вадим Гатауллин on 25/03/2019.
+//  Copyright © 2019 Вадим Гатауллин. All rights reserved.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ExamWork/webViewController.swift
@@ -0,0 +1,30 @@
+//
+//  webViewController.swift
+//  ExamWork
+//
+//  Created by Вадим Гатауллин on 25/03/2019.
+//  Copyright © 2019 Вадим Гатауллин. All rights reserved.
+//
+
+import UIKit
+
+class webViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
